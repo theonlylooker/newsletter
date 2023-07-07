7a6b3a84f6acdff9d5eca21f85b5b0b37ff4408c
basurco eduardo <angelsoulsword@gmail.com>
2023-07-07 12:25:20 -0500

    feat: add components index

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./button";
+export { Header } from "./header";
+export { InputText } from "./input-text";
+export { ListItem } from "./list-item";
+export { Paragraph } from "./paragraph";
