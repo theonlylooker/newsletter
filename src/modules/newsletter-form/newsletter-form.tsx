1c0a64859eafa48ec45e5a80cb8039f20c0ebcfe
basurco eduardo <angelsoulsword@gmail.com>
2023-07-07 15:09:56 -0500

    feat: add newsletter form component mobile

--- src/modules/newsletter-form/newsletter-form.tsx
+++ src/modules/newsletter-form/newsletter-form.tsx
@@ -1,3 +1,31 @@
+import { BgMobile } from "../../assets";
+import {
+  Button,
+  Header,
+  InputText,
+  ListItem,
+  Paragraph,
+} from "../../components";
+
 export const NewsletterForm = () => {
-  return <div>form</div>;
+  return (
+    <div>
+      <BgMobile />
+      <div className="pt-10 px-5 flex flex-col gap-5">
+        <Header size="lg">Stay Updated!</Header>
+        <Paragraph>
+          Join 60,000+ product managers receiving monthly updates on:
+        </Paragraph>
+        <ul>
+          <ListItem>Product discovery and building what matters </ListItem>
+          <ListItem>Measuring to ensure updates are a success </ListItem>
+          <ListItem>And much more! </ListItem>
+        </ul>
+        <form className="pt-5 flex flex-col gap-5" action="">
+          <InputText id="email">Email address</InputText>
+          <Button size="large">Subscribe to monthly newsletter</Button>
+        </form>
+      </div>
+    </div>
+  );
 };
