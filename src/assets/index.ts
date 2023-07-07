e9c0c5f1d747e9362a0e5e2138d0a6e7966e45e5
basurco eduardo <angelsoulsword@gmail.com>
2023-07-07 12:23:23 -0500

    feat: add svg index

---
+++ src/assets/index.ts
@@ -0,0 +1,5 @@
+export { ReactComponent as Check } from "./icon-list.svg";
+export { ReactComponent as Success } from "./icon-success.svg";
+export { ReactComponent as BgDesktop } from "./illustration-sign-up-desktop.svg";
+export { ReactComponent as BgMobile } from "./illustration-sign-up-mobile.svg";
+export { ReactComponent as Logo } from "./react.svg";
